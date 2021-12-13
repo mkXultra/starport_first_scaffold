08f3c9f916d899822e39085d16707e4b4c12baff
mkXultra <eeapprenctice@gmail.com>
2021-12-13 21:45:37 +0900

    execute scaffold message

---
+++ vue/src/store/generated/mkXultra/starport_first_scaffold/mkXultra.starportfirstscaffold.starportfirstscaffold/module/types/starportfirstscaffold/query.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export const protobufPackage = "mkXultra.starportfirstscaffold.starportfirstscaffold";
+export class QueryClientImpl {
+    constructor(rpc) {
+        this.rpc = rpc;
+    }
+}
